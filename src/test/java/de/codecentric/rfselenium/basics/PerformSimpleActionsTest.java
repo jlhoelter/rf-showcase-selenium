661616e511887d274ad75950eb32b87234e4e963
jlhoelter <jan@musiedlak.de>
2014-11-05 06:56:23 +0100

    Example for Simple Actions

--- src/test/java/de/codecentric/rfselenium/basics/PerformSimpleActionsTest.java
+++ src/test/java/de/codecentric/rfselenium/basics/PerformSimpleActionsTest.java
@@ -1,5 +1,8 @@
 package de.codecentric.rfselenium.basics;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 import org.hamcrest.CoreMatchers;
 import org.junit.After;
 import org.junit.Assert;
@@ -23,17 +26,39 @@ public class PerformSimpleActionsTest {
 	@Test
 	public void testUseSimpleCommandButton(){
 		openCommandButtonSection();
-		
+		String myName = "Jan";
+		enterInInputField(myName);
+		submit();
+		assertOutputTextIs("Hello "+myName+" !");		
+	}
+
+	private void assertOutputTextIs(String expected) {
+		// Don't do it that way
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		String actualText = driver.findElement(By.className("outhello")).getText();
+		assertThat(actualText, equalTo(expected));
+	}
+
+	private void submit() {
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
+	}
+
+	private void enterInInputField(String text) {
+		driver.findElement(By.xpath("//input[@type='text']")).sendKeys(text);
 	}
 
 
 	private void openCommandButtonSection() {
 		driver.findElement(By.id("j_idt22")).click();
-		assertTabExistsWithCaption("Command Button Simple");
+//		assertTabExistsWithCaption("Command Button Simple");
 	}
 
 	private void assertTabExistsWithCaption(String caption) {
-		Assert.assertThat(driver.findElement(By.cssSelector(".rf-tbp.navigation")).getText(),CoreMatchers.equalTo(caption));
+		Assert.assertThat(driver.findElement(By.cssSelector(".rf-tab-hdr-tabs .rf-tab-lbl")).getText(),CoreMatchers.equalTo(caption));
 	}
 
 	WebDriver initDriver() {
